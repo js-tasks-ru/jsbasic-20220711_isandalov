c9c9973f9e1d258811a969ff5a89e0975ea49f1c
Yan Sandalov <isandalov@mail.ru>
2022-07-18 08:32:20 +0300

    решение модуль 1

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,8 @@
 function truncate(str, maxlength) {
-  // ваш код...
+  if (str && maxlength && str.length <= maxlength)
+    return str
+  else if (str && maxlength && str.length > maxlength)
+    return str.slice(0, maxlength-1) + '…'
+  else
+    return 'данные не корректны'
 }
