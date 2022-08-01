27db0e0423961518d2c6f06ddbbf143165628487
Yan Sandalov <isandalov@mail.ru>
2022-08-01 23:48:44 +0300

    решение модуль 4

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,26 @@
 function highlight(table) {
-  // ваш код...
+  for (let i = 0; i < table.rows.length; i += 1) {
+        
+    if ( table.rows[i].cells[3].dataset.available == "true") {
+      table.rows[i].classList.add('available') 
+      }
+    else if ( table.rows[i].cells[3].dataset.available == "false")  {
+      table.rows[i].classList.add('unavailable') 
+      }
+    else {
+      table.rows[i].setAttribute('hidden','')
+      }
+      
+    if ( table.rows[i].cells[2].textContent == "m" ) {
+       table.rows[i].classList.add('male') 
+    }
+    else if ( table.rows[i].cells[2].textContent == "f" ) {
+      table.rows[i].classList.add('female') 
+    }
+
+    if (parseInt( table.rows[i].cells[1].textContent, 10 ) < 18 ) {
+     table.rows[i].style.textDecoration = "line-through"
+    }
+
+  }
 }
